025c63955910fb580ec483474235ce7e02b1a6f5
ViktorBelskih <belskih15@gmail.com>
2023-10-13 18:21:44 +0700

    Static

--- agrochem19/settings.py
+++ agrochem19/settings.py
@@ -132,12 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Замените 'your_app' на название вашего Django-приложения
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static'),  # Замените 'your_app' на название вашего Django-приложения
+#]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
