f8734897d3a93b352a505743740d956a43e29a3b
ViktorBelskih <belskih15@gmail.com>
2023-10-14 17:01:13 +0700

    media

--- agrochem19/urls.py
+++ agrochem19/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +26,12 @@ urlpatterns = [
     path("user/", include("user.urls")),
     path("admin/", admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', static, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
